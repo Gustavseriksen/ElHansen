6aa02d2aef7b160b99378cbb38f54f22151d3d69
Gustavseriksen <guer0001@stud.kea.dk>
2025-11-25 13:43:05 +0100

    Initial commit

---
+++ src/main/java/org/example/elhansen/ElHansenApplication.java
@@ -0,0 +1,13 @@
+package org.example.elhansen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElHansenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElHansenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/elhansen/ElHansenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elhansen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElHansenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
